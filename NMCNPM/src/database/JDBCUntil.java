0c3298a2d7b2d8b8d604bb7fdab9d5902b77a3fa
Tu <tudxtworkspace@gmail.com>
2024-04-13 16:35:17 +0700

    JDBC

--- NMCNPM/src/database/JDBCUntil.java
+++ NMCNPM/src/database/JDBCUntil.java
@@ -12,10 +12,16 @@ public class JDBCUntil {
 
         try {
             // Load the SQL Server driver (not needed to register explicitly)
+		// Tai SQL Sever de ket noi
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
             // Connection parameters
+<<<<<<< HEAD
             String url = "jdbc:sqlserver://DESKTOP-KT3KI6S:1433;encrypt=true;databaseName=Quanlychungcu;trustServerCertificate = true";
+=======
+		//DESKTOP-KT3KI6S:1433 ten may tinh local ket noi, trong SQL sever tao 1 database la NMCNPM
+            String url = "jdbc:sqlserver://DESKTOP-KT3KI6S:1433;encrypt=true;databaseName=NMCNPM;trustServerCertificate = true";
+>>>>>>> ea393dd13a27c5f1dcf5e83273bff637a480abf5
             String userName = "sa";
             String password = "admin";
 

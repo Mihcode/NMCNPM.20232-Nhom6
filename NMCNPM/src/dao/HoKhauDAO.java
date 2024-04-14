54f13a03ae03e3deae4ec49370e4e2122c2b35cc
Tu <tudxtworkspace@gmail.com>
2024-04-14 20:14:40 +0700

    them KhoanThuView

---
+++ NMCNPM/src/dao/HoKhauDAO.java
@@ -0,0 +1,49 @@
+package dao;
+
+import java.util.ArrayList;
+import model.HoKhau;
+
+public class HoKhauDAO implements DAOInterface<HoKhauDAO> {
+	@Override
+	public int insert(HoKhauDAO t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(HoKhauDAO t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(HoKhauDAO t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public HoKhauDAO selectByusername(HoKhauDAO t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkExit(HoKhauDAO t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<HoKhauDAO> selectALL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<HoKhauDAO> selectByCondition(String condition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
